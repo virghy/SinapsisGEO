73f28a61af417e50a71fb4a5e13da06ee10027fe
Virginio Gonzalez <virginiogonzalez@hotmail.com>
2015-05-11 18:54:03 -0400

    - Permite personalizar las cantidades en los combos - Agranda tu combo. Falta todavia funcionar - Enlace para actualizar los combos y precios

---
+++ PH/Entidades/SP_CALL_COMBO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PH.Entidades
+{
+    public class SP_CALL_COMBO
+    {
+    //IDPROMO Varchar(14),
+    //PROMO Varchar(200),
+    //IDDEFINICION_PROMO Integer,
+    //DEFINICION_PROMO Varchar(100),
+    //CANTIDAD_DEFINICION Integer,
+    //IDPRODUCTO_CMB Integer,
+    //IDPRODUCTO Varchar(14),
+    //PRODUCTO Varchar(200),
+    //CANTIDAD Double precision,
+    //PREDETERMINADO Integer,
+    //ESTADO Integer,
+    //COMBINACIONES Double precision,
+    //AGRANDADO Varchar(14),
+    //IDART_COSTO_AGRANDADO Varchar(14) )
+        public string IDPROMO { get; set; }
+        public string PROMO { get; set; }
+        public int IDDEFINICION_PROMO { get; set; }
+        public string DEFINICION_PROMO { get; set; }
+        public int CANTIDAD_DEFINICION { get; set; }
+        public int IDPRODUCTO_CMB { get; set; }
+        public string IDPRODUCTO { get; set; }
+        public string PRODUCTO { get; set; }
+        public decimal CANTIDAD { get; set; }
+        public int PREDETERMINADO { get; set; }
+        public int? ESTADO { get; set; }
+        public int? COMBINACIONES { get; set; }
+        public String AGRANDADO { get; set; }
+        public string IDART_COSTO_AGRANDADO { get; set; }
+    }
+}
---
+++ PH/Entidades/SP_CALL_PRECIO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PH.Entidades
+{
+    public class SP_CALL_PRECIO
+    {
+        public string IDARTICULO { get; set; }
+        public decimal PRECIO { get; set; }
+        public int IDLISTA { get; set; }
+        public string LISTA { get; set; }
+    }
+}
